0b6c5fe81d921d30514d1d80f9f74d5af39844ed
Jelvis <jelvis2016@gmail.com>
2016-12-01 21:19:48 +0800

    Update gen_Asiaip.sh

--- gen_Asiaip.sh
+++ gen_Asiaip.sh
@@ -6,13 +6,18 @@ fi
 
 cat /dev/null > "ss-iptables"
 echo "#!/bin/sh" >> "ss-iptables"
+#先删除默认全部走代理的链
+echo "iptables -t nat -D SS_SPEC_WAN_FW 1" >> "ss-iptables"
 echo "iptables -t nat -N SHADOWSOCKS" >> "ss-iptables"
+#代理服务器
 echo "iptables -t nat -A SHADOWSOCKS -d 23.106.131.215 -j RETURN" >> "ss-iptables"
+#内网ip
 echo "iptables -t nat -A SHADOWSOCKS -d 192.168.31.123 -j RETURN" >> "ss-iptables"
 echo "iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN" >> "ss-iptables"
 echo "iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN" >> "ss-iptables"
 echo "iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/16 -j RETURN" >> "ss-iptables"
 echo "iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN" >> "ss-iptables"
+#dns
 echo "iptables -t nat -A SHADOWSOCKS -d 114.114.114.114 -j RETURN" >> "ss-iptables"
 echo "iptables -t nat -A SHADOWSOCKS -d 223.5.5.5 -j RETURN" >> "ss-iptables"
 echo "iptables -t nat -A SHADOWSOCKS -d 8.8.8.8 -j RETURN" >> "ss-iptables"
@@ -37,7 +42,6 @@ FS="|"
 }'
 
 echo "iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 1080" >> "ss-iptables"
-echo "iptables -t nat -D SS_SPEC_WAN_FW 1" >> "ss-iptables"
 echo "iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS" >> "ss-iptables"
 echo "iptables -t nat -A OUTPUT -p tcp -j SHADOWSOCKS" >> "ss-iptables"
 
